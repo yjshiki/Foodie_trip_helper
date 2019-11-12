67c8883c701383943048dc45b27839c8238f7947
Li Dinghong <networkz2019@gmail.com>
2019-11-11 21:12:24 -0500

    trusted_reviews now works

--- sql/trusted_reviews.sql
+++ sql/trusted_reviews.sql
@@ -4,67 +4,63 @@
 -- Has yelping_since before 2017 
 -- Has never reviewed a business more than 2 times within one year. 
 -- The number of reviews he has given to 
--- any business not opening on Saturday
--- does not exceed 20% of the total number of reviews given by him. 
+-- any business does not exceed 20% of the total number of reviews he has given. 
 -- Has made more than 2 useful reviews
 -- (Numbers may be changed according to the data)
 -- Schema: (user_name, review)
 -- ==> yelp_review2.csv <==
 -- "review_id","user_id","business_id","stars","date","text","useful","funny","cool"
 select 
-	a.user_name as user_name, 
-	b.text as review
+	a.user_id as user_id, 
+	a.name as user_name, 
+	b.review as review
 from yelp_user a
 join (
 	select 
 		b.user_id as user_id, 
-		c.business_id as business_id
+		c.business_id as business_id, 
+		b.text as review, 
+		c.name as buiness_name
 	from yelp_review2 b
 	join yelp_business c 
 	on b.business_id = c.business_id
 ) b 
 on a.user_id = b.user_id
 where 
-	b.buiness_name = "Dental by Design"
+	b.buiness_name = "Philthy Philly's"
 	-- where a.name = "${inputName}"
-	and a.yelping_since <= 2017 
-	and 0.2 * a.review_count >= all(
-		select t.review_count_business
-		from (
-			select 
-				x.business_id as business_id, 
-				count(x.review_id) as review_count_business
-			from yelp_review2 x
-			join yelp_business_hours y
-			on x.business_id = y.business_id
-			where x.user_id = a.user_id 
-			and y.saturday = "None"
-			group by business_id
-		) t
-	)
 	and (
-		select count(*)
-		from yelp_review2 x
-		where x.user_id = a.user_id
-		and x.useful = 1
-	) >= 2
-	and 2 >= all(
-		select t.review_count_business 
-		from (
-			select 
-				x.business_id as business_id, 
-				extract(year from x.date) as year, 
-				count(x.review_id) as review_count_business
+		a.yelping_since <= 2017 
+		or 0.2 * a.review_count >= all(
+			select t.review_count_business
+			from (
+				select 
+					x.business_id as business_id, 
+					count(x.review_id) as review_count_business
+				from yelp_review2 x 
+				where x.user_id = user_id
+				group by business_id
+			) t
+		)
+		or (
+			select count(*)
 			from yelp_review2 x
-			where x.user_id = a.user_id
-			group by business_id, year
-		) t 
+			where x.user_id = user_id
+			and x.useful = 1
+		) >= 2
+		or 2 >= all(
+			select t.review_count_business 
+			from (
+				select 
+					x.business_id as business_id, 
+					extract(year from x.date) as year, 
+					count(x.review_id) as review_count_business
+				from yelp_review2 x
+				where x.user_id = user_id
+				group by business_id, year
+			) t 
+		)
 	)
-order by length(review) desc, user_name
-limit 10;
-
-
-
-
-
+order by user_name
+limit 100;
 
