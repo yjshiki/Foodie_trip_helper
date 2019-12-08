9445e59262096a1cb4099ba85c8e22d0839092b6
Li Dinghong <networkz2019@gmail.com>
2019-12-08 16:28:50 -0500

    optimize trusted_reviews

--- sql/trusted_reviews.sql
+++ sql/trusted_reviews.sql
@@ -1,5 +1,5 @@
 -- Input the name of a business 
--- Print top 10 reviews from all trusted users for this business. 
+-- Print the 10 latest reviews from all trusted users for this business. 
 -- A ``trusted user'' is defined as a user meeting one of the following: 
 -- Has yelping_since before 2017 
 -- Has never reviewed a business more than 2 times within one year. 
@@ -10,26 +10,18 @@
 -- Schema: (user_name, review)
 -- ==> yelp_review2.csv <==
 -- "review_id","user_id","business_id","stars","date","text","useful","funny","cool"
+-- before: 29.89 sec, after: 0.04 sec
 select 
 	a.user_id as user_id, 
 	a.name as user_name, 
+	b.review_date as review_date, 
 	b.review as review
-from yelp_user a
-join (
+from (
 	select 
-		b.user_id as user_id, 
-		c.business_id as business_id, 
-		b.text as review, 
-		c.name as buiness_name
-	from yelp_review2 b
-	join yelp_business c 
-	on b.business_id = c.business_id
-) b 
-on a.user_id = b.user_id
-where 
-	b.buiness_name = "Philthy Philly's"
-	-- where a.name = "${inputName}"
-	and (
+		a.user_id as user_id, 
+		a.name as name 
+	from yelp_user a 
+	where 
 		a.yelping_since <= 2017 
 		or 0.2 * a.review_count >= all(
 			select t.review_count_business
@@ -60,7 +52,21 @@ where
 				group by business_id, year
 			) t 
 		)
-	)
-order by user_name
-limit 100;
+) a
+join (
+	select 
+		b.user_id as user_id, 
+		c.business_id as business_id, 
+		b.text as review, 
+		b.date as review_date, 
+		c.name as buiness_name
+	from yelp_review2 b
+	join yelp_business c 
+	on b.business_id = c.business_id
+) b 
+on a.user_id = b.user_id
+where b.buiness_name = "Philthy Philly's"
+order by review_date desc
+limit 10;
+-- where a.name = "${inputName}"
 
