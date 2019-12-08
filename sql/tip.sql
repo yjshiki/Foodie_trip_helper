11bb3e14c06d6c693c57d832efd09ee1795bdb48
Li Dinghong <networkz2019@gmail.com>
2019-12-08 18:16:49 -0500

    great opt in tip.sql

--- sql/tip.sql
+++ sql/tip.sql
@@ -5,73 +5,36 @@
 -- Order first by highest “stars”, then highest “review_count” of restaurants, 
 -- the date of the tip descendingly, and then the user_name. 
 -- Schema: (business_id, business_name, user_id, user_name, tip, date, stars, review_count)
--- selected businesses
-with Businesses as (
-	-- genres of the input business
-	with G as (
-		select b.category  
-		from yelp_categories b
-		join yelp_business a
-		on b.business_id = a.business_id 
-		-- where a.name = "${inputName}"
-		where a.name = "Dental by Design"
-	)
-	select distinct 
-		a.business_id as business_id, 
-		a.name as business_name, 
-		a.stars as stars, 
-		a.review_count as review_count
-	from yelp_business a
-	-- if G is a subset of (genres of a) 
-	where (
-		not(exists(
-			select t.category 
-			-- G
-			from (
-				select b.category  
-				from yelp_categories b
-				join yelp_business a
-				on b.business_id = a.business_id 
-				-- where a.name = "${inputName}"
-				where a.name = "Dental by Design"
-			) t 
-			where t.category != all(
-				select distinct b.category 
-				from yelp_categories b 
-				-- join (
-				-- 	select * 
-				-- 	from yelp_business x
-				-- 	where x.business_id = business_id
-				-- ) c
-				-- on b.business_id = c.business_id 
-				where a.business_id = b.business_id
-			)
-		))
-	-- ) and a.name != "${inputName}"
-	) and a.name != "Dental by Design"
-	and a.is_open = 1
-	order by stars desc, review_count desc, business_name
-	limit 3
-)
--- select distinct 
--- 	-- a.business_id as business_id, 
--- 	a.name as business_name, 
--- 	-- b.user_id as user_id, 
--- 	b.user_name as user_name, 
--- 	b.tip as tip, 
--- 	a.stars as stars, 
--- 	a.review_count as review_count
--- from Businesses a
--- join (
--- 	select 
--- 		t.user_id as user_id, 
--- 		t.text as tip, 
--- 		x.name as user_name
--- 	from yelp_tip t
--- 	join yelp_user x
--- 	on t.user_id = x.user_id
--- ) b
--- on a.business_id = b.business_id
--- order by 
--- stars desc, review_count desc, date desc, user_name;
+-- before: > 4 h, after: 0.54 sec
+select distinct 
+	a.business_id as business_id, 
+	a.name as business_name, 
+	a.stars as stars, 
+	a.review_count as review_count
+from yelp_business a
+-- if G is a subset of (genres of a) 
+where (
+	not(exists(
+		select t.category 
+		-- G: genres of the input business
+		from (
+			select b.category  
+			from yelp_categories b
+			join yelp_business a
+			on b.business_id = a.business_id 
+			-- where a.name = "${inputName}"
+			where a.name = "Dental by Design"
+			limit 1000000000000000
+		) t 
+		where t.category != all(
+			select distinct b.category 
+			from yelp_categories b 
+			where a.business_id = b.business_id
+		)
+	))
+-- ) and a.name != "${inputName}"
+) and a.name != "Dental by Design"
+and a.is_open = 1
+order by stars desc, review_count desc, business_name
+limit 3
 
